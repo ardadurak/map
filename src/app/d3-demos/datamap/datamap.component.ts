cc54cdf113a988e8c4d6c2e32321072acfd56aac
Arda Macbook <arda.durak@gmail.com>
2017-07-02 01:24:23 +0100

    Responsiveness, styling fixes (pre final)

--- src/app/d3-demos/datamap/datamap.component.ts
+++ src/app/d3-demos/datamap/datamap.component.ts
@@ -1,21 +1,6 @@
 import { Stocks, MapData } from '../shared';
-
 import { Component, ElementRef, Input, NgZone, OnChanges, OnDestroy, OnInit, SimpleChange } from '@angular/core';
-
-
-import {
-  D3Service,
-  D3,
-  Axis,
-  BrushBehavior,
-  BrushSelection,
-  D3BrushEvent,
-  ScaleLinear,
-  ScaleOrdinal,
-  Selection,
-  Transition
-} from 'd3-ng2-service';
-
+import { D3Service, D3, Axis, BrushBehavior, BrushSelection, D3BrushEvent, ScaleLinear, ScaleOrdinal, Selection, Transition} from 'd3-ng2-service';
 import * as Datamap from 'datamaps';
 
 @Component({
@@ -40,36 +25,144 @@ export class DatamapComponent implements OnInit, OnChanges, OnDestroy {
     this.d3 = d3Service.getD3();
     this.parentNativeElement = element.nativeElement;
   }
+
   ngOnDestroy() {
     if (this.d3Svg.empty && !this.d3Svg.empty()) {
       this.d3Svg.selectAll('*').remove();
     }
   }
-  ngOnChanges(changes: { [propKey: string]: SimpleChange }) {
-      console.log('here too');
-      if (
-        (changes['width'] && !changes['width'].isFirstChange()) ||
-        (changes['height'] && !changes['height'].isFirstChange()) ||
-        (changes['phylloRadius'] && !changes['phylloRadius'].isFirstChange()) ||
-        (changes['pointRadius'] && !changes['pointRadius'].isFirstChange())
-      ) {
-        if (this.d3Svg.empty && !this.d3Svg.empty()) {
-          this.changeLayout();
-        }
-      }
-    }
+
   ngOnInit() {
     let self = this;
     let d3 = this.d3;
     let d3ParentElement: Selection<HTMLElement, any, null, undefined>;
     let d3Svg: Selection<SVGSVGElement, any, null, undefined>;
     let d3G: Selection<SVGGElement, any, null, undefined>;
-   var map = new Datamap({
+    let svgWidth: number, svgHeight: number;
+    const xFactorUk = 0.3795136671202318, yFactorUk = 0.608870085225356;
+    const xFactorUs = 0.4859626225320041, yFactorUs = 0.16291388766840606;
+    let map = this.initMap();
+    let grayColor = '#747474';
+    let data = [ 
+      { "Region": "GBR", "Group1": 10,"Group2": 20,"x": 525,"y": 99 },
+      { "Region": "USA","Group1": 10,"Group2": 10, "x": 410, "y": 370 }
+    ];
+
+    d3ParentElement = d3.select(this.parentNativeElement);
+    d3Svg = this.d3Svg = d3ParentElement.select<SVGSVGElement>('svg');
+    svgWidth = parseFloat( d3Svg.style("width"));
+    svgHeight = parseFloat(d3Svg.style('height'));
+    let xUk = svgWidth * xFactorUk, xUs = svgWidth * xFactorUs;
+    let yUk = svgHeight * yFactorUk, yUs = svgHeight * yFactorUs;
+    data[0].x = xUk;
+    data[0].y = yUk;
+    data[1].x = xUs;
+    data[1].y = yUs;   
+
+    d3Svg.append("circle")
+      .attr("cx", xUk)
+      .attr("cy", yUk)
+      .attr("r", 50)
+      .attr("fill", grayColor)
+      .attr('class', 'circle-uk');
+    
+    d3Svg.append("circle")
+      .attr("cx", xUs) 
+      .attr("cy", yUs)
+      .attr("r", 50)
+      .attr("fill", grayColor)
+      .attr('class', 'circle-us');
+
+    var arc : any= d3.arc()
+       .innerRadius(20).outerRadius(40);
+
+    var pie = d3.pie()
+      .value(function(d: any){ return d });
+ 
+    var pies = d3Svg.selectAll('.pie')
+      .data(data)
+      .enter()
+      .append<SVGGElement>('g')
+      .attr('class', 'pie')
+      .attr("transform", function(d) {
+        return "translate(" + d.x + "," + d.y + ")";
+     });
+
+    var color = d3.scaleOrdinal(d3.schemeCategory10);
+    
+    pies.selectAll('.slice')
+      .data(function(d){
+      return pie([d.Group1, d.Group2]); })
+      .enter()
+      .append<SVGGElement>('path')
+      .attr('d',  arc)
+      .attr('cursor', 'pointer')
+      .style('fill', function(d,i: any){
+        return color(i);
+      })
+      .on("mouseover",function(d:any,i) {
+          /*pies.select(this).append("text")
+            .attr("dy", ".5em")
+            .style("text-anchor", "middle")
+            .style("fill", function(d:any,i){return "black";})
+            .text(d.data)*/
+        })
+      .on("mouseout", function(d) {
+           pies.select("text").remove();
+      })
+      .on("click", function(d) {
+        //pies.selectAll('.pie').transition().duration(750).attr("d", arcInitial);
+          d3.select(this).transition()
+              .duration(750)
+              .attr("d", arcFinal);
+        });
+
+      var arcInitial = d3.arc()
+				.startAngle(function(d){ return d.startAngle; })
+				  .endAngle(function(d){ return d.endAngle; })
+				  .innerRadius(20)
+				  .outerRadius(40);
+       var arcFinal = d3.arc()
+					.startAngle(function(d){ return d.startAngle; })
+				  	.endAngle(function(d){ return d.endAngle; })
+				  	.innerRadius(10)
+				  	.outerRadius(40);  
+
+  
+    window.addEventListener('resize', function(event){
+      
+      map.resize();
+      svgWidth = parseFloat( d3Svg.style("width"));
+      svgHeight = parseFloat(d3Svg.style('height'));
+      let xUk = svgWidth * xFactorUk, xUs = svgWidth * xFactorUs;
+      let yUk = svgHeight * yFactorUk, yUs = svgHeight * yFactorUs;
+      data[0].x = xUk;
+      data[0].y = yUk;
+      data[1].x = xUs;
+      data[1].y = yUs;   
+
+      d3Svg.select('.circle-uk')
+        .attr("cx", xUk)
+        .attr("cy", yUk)
+
+      d3Svg.select('.circle-us')
+        .attr("cx", xUs) 
+        .attr("cy", yUs)
+      
+      d3Svg.selectAll('.pie')
+        .data(data)
+        .attr("transform", function(d) {
+          return "translate(" + d.x + "," + d.y + ")";
+      });
+    });  
+  }
+  private initMap(){
+        var map =  new Datamap({
 
         element: document.getElementById('container'),
         projection: 'mercator',
         responsive: true,
-        highlightFillColor: '#0091DA',
+        highlightFillColor: 'defaultFill',
         geographyConfig: {
             borderWidth: 0
         },
@@ -136,103 +229,8 @@ export class DatamapComponent implements OnInit, OnChanges, OnDestroy {
             }
         }
       ],  {strokeWidth: 1, arcSharpness: 1.4});
-
-       var data = [
-            {
-              "Region": "GBR",
-              "Group1": 10,
-              "Group2": 20,
-              "Group3": 30,
-              "Group4": 40,
-              "lat": 50,
-              "lng": -35
-            },
-            {
-              "Region": "USA",
-              "Group1": 10,
-              "Group2": 10,
-              "Group3": 10,
-              "Group4": 10,
-              "lat": -20,
-              "lng": -70
-            }
-          ];
-      // add arrow http://bl.ocks.org/dem42/e10e933990ee662c9cbd
-      d3ParentElement = d3.select(this.parentNativeElement);
-      d3Svg = this.d3Svg = d3ParentElement.select<SVGSVGElement>('svg');
-
-      d3Svg.append("circle")
-        .attr("cx", 297)
-        .attr("cy", 304)
-        .attr("r", 50)
-        .attr("fill", '#747474');
-
-    d3Svg.append("circle")
-        .attr("cx", 388) 
-        .attr("cy", 99)
-        .attr("r", 50)
-        .attr("fill", '#747474');
-
-       var arc : any= d3.arc()
-       .innerRadius(20).outerRadius(40),               
-      pie = d3.pie()
-      .value(function(d: any){ return d });
-
-      var projection = d3.geoProjection(function(x, y) {
-        return [x, Math.log(Math.tan(Math.PI / 4 + y / 2))];
-      });
- 
-    var pies = d3Svg.selectAll('.pie')
-      .data(data)
-      .enter()
-      .append<SVGGElement>('g')
-      .attr('class', 'pie')
-      .attr("transform", function(d) {
-        return "translate(" + projection([d.lng, d.lat])[0] + "," + projection([d.lng, d.lat])[1] + ")";
-        //return "translate(150px, 300px)";
-     });
-
-    var color = d3.scaleOrdinal(d3.schemeCategory10);
-    
-    pies.selectAll('.slice')
-      .data(function(d){
-      return pie([d.Group1, d.Group2, d.Group3, d.Group4]); })
-      .enter()
-      .append<SVGGElement>('path')
-      .attr('d',  arc)
-      .attr('cursor', 'pointer')
-      .style('fill', function(d,i: any){
-        return color(i);
-      })
-      .on("mouseover",function(d:any,i) {
-          /*pies.select(this).append("text")
-            .attr("dy", ".5em")
-            .style("text-anchor", "middle")
-            .style("fill", function(d:any,i){return "black";})
-            .text(d.data)*/
-        })
-      .on("mouseout", function(d) {
-           pies.select("text").remove();
-      })
-      .on("click", function(d) {
-        //pies.selectAll('.pie').transition().duration(750).attr("d", arcInitial);
-          d3.select(this).transition()
-              .duration(750)
-              .attr("d", arcFinal);
-        });
-
-      var arcInitial = d3.arc()
-				.startAngle(function(d){ return d.startAngle; })
-				  .endAngle(function(d){ return d.endAngle; })
-				  .innerRadius(20)
-				  .outerRadius(40);
-       var arcFinal = d3.arc()
-					.startAngle(function(d){ return d.startAngle; })
-				  	.endAngle(function(d){ return d.endAngle; })
-				  	.innerRadius(10)
-				  	.outerRadius(40);   
-            
-  }
+        return map;
+      }
     private changeLayout() {
       console.log('heey change');
       /*
@@ -246,6 +244,20 @@ export class DatamapComponent implements OnInit, OnChanges, OnDestroy {
       .attr('cx', function (d) { return d.x; })
       .attr('cy', function (d) { return d.y; })
       .attr('r', this.pointRadius);*/
+      
 
   }
+  ngOnChanges(changes: { [propKey: string]: SimpleChange }) {
+      console.log('here too');
+      if (
+        (changes['width'] && !changes['width'].isFirstChange()) ||
+        (changes['height'] && !changes['height'].isFirstChange()) ||
+        (changes['phylloRadius'] && !changes['phylloRadius'].isFirstChange()) ||
+        (changes['pointRadius'] && !changes['pointRadius'].isFirstChange())
+      ) {
+        if (this.d3Svg.empty && !this.d3Svg.empty()) {
+          this.changeLayout();
+        }
+      }
+    }
 }
